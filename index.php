ca3ee6a0d6ae32b3b3a8cacc4fa81b9199e3da82
ThankGod Okoro <thankgodokoro@ThankGods-MBP.lan>
2022-10-14 11:11:45 +0100

    new interface

--- index.php
+++ index.php
@@ -31,46 +31,130 @@ include './components/navbar.php';
       </section>
     </div>
 
-    <section class="py-15 py-xl-20">
-      <div class="container">
-        <div class="row mb-10">
-          <div class="col-lg-10">
-            <span class="eyebrow text-primary mb-4">About Us</span>
-            <h3 class="fs-4">We connect you to the right lender combine personal and online underwriting to make sure we can help every single business owner.</h3>
-          </div>
-        </div>
-      </div>
-    </section>
-
-      <section class="py-15 py-xl-20">
+      <section class="py-7 mt-5 mb-0">
         <div class="container">
-          <div class="row align-items-center justify-content-between">
-            <div class="col-lg-6 mb-5 mb-lg-0">
-              <div class="frame">
-                <img src="assets/images/pic1.jpg" class="img-fluid" alt="">
-                <span></span>
-              </div>
-            </div>
-            <div class="col-lg-6 col-xl-5">
-              <h2 class="fw-bold">Custom Funding Solution</h2>
-              <p class="text-secondary">NAS Consulting Inc. works with finders that provides customized funding solutions from $1 million to $100 billon across the capital stack. Project funding for real estate, energy, infrastructure, hospitality and more at very low Interest rate.</p>
+          <div class="row mb-10">
+            <div class="col-lg-10">
+              <h3 class="fs-4">We connect you to the right lender combine personal and online underwriting to make sure we can help every single business owner.</h3>
             </div>
           </div>
         </div>
       </section>
 
 
-      <section class="mb-9">
-        <div class="container">
-          <div class="row align-items-center justify-content-between">
-            <div class="col-lg-6 mb-5 col-xl-5">
-              <h2 class="fw-bold">$500 Billion+ funded by our network</h2>
-              <p class="text-secondary">NAS Consulting Inc. works with finders that provides customized funding solutions from $1 million to $100 billon across the capital stack. Project funding for real estate, energy, infrastructure, hospitality and more at very low Interest rate.</p>
+      <section class="bg-black mx-xl-3">
+        <div class="container py-15 pb-10 py-xl-20 level-3">
+          <div class="row">
+            <div class="col-lg-6 inverted">
+              <h2 class="fw-bold">Pre-qualify your company</h2>
+              <ul class="list-group list-group-minimal">
+                <li class="list-group-item d-flex align-items-center">
+                  <div class="icon-box icon-box-sm bg-opaque-white rounded-circle me-2"><i class="bi bi-check2 text-white"></i>
+                  </div>
+                  <b>One lender, great options:</b> Borrow 50,000 to $5,000,000. Payments are flexible (secured/unsecured).
+                </li>
+                <li class="list-group-item d-flex align-items-center">
+                  <div class="icon-box icon-box-sm bg-opaque-white rounded-circle me-2"><i class="bi bi-check2 text-white"></i>
+                  </div>
+                  <b>Streamlined process:</b> Complete one free form in just 2 minutes to receive lenders offers for your business.
+                </li>
+                <li class="list-group-item d-flex align-items-center">
+                  <div class="icon-box icon-box-sm bg-opaque-white rounded-circle me-2"><i class="bi bi-check2 text-white"></i>
+                  </div>
+                  <b>Get the money you need:</b> To expand, finance contract, purchase equipment, mortgage and lands finance/refinance.
+                </li>
+                <li class="list-group-item d-flex align-items-center">
+                  <div class="icon-box icon-box-sm bg-opaque-white rounded-circle me-2"><i class="bi bi-check2 text-white"></i>
+                  </div>
+                  <b>Email:</b> finance@nascocanada.ca
+                </li>
+                <li class="list-group-item d-flex align-items-center">
+                  <div class="icon-box icon-box-sm bg-opaque-white rounded-circle me-2"><i class="bi bi-check2 text-white"></i>
+                  </div>
+                  <b>Phone number:</b>  450-499-2537
+                </li>
+                <li class="list-group-item d-flex align-items-center">
+                  <div class="icon-box icon-box-sm bg-opaque-white rounded-circle me-2"><i class="bi bi-check2 text-white"></i>
+                  </div>
+                  <b>Address:</b> 210-303 Lowe Road, Saskatoon SK S7S 1P2, Canada
+                </li>
+              </ul>
             </div>
-            <div class="col-lg-6 mb-lg-0">
-              <div class="frame">
-                <img src="assets/images/pic2.jpg" class="img-fluid" alt="">
-                <span></span>
+          </div>
+        </div>
+        <div class="container-fluid back back-static">
+          <div class="row justify-content-end h-100">
+            <div class="col-lg-6 overflow-hidden position-relative">
+              <figure class="background background-parallax skrollable skrollable-between" style="background-image: url(assets/images/company.jpg); transform: translateY(11.5101%);" data-bottom-top="transform: translateY(0%);" data-top-bottom="transform: translateY(20%);">
+              </figure>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+
+      <section class="py-15 py-xl-20">
+        <div class="container">
+          <div class="row align-items-center justify-content-center justify-content-xl-between">
+            <div class="col-lg-8 col-xl-6">
+              <div class="row g-2 gallery-1">
+                <div class="col aos-init aos-animate" data-aos="fade-up">
+                  <figure class="media media-image equal-md-1-2" data-bp="{&quot;imgSrc&quot;: &quot;./assets/images/startup-3.jpg&quot; , &quot;parentGalleryClass&quot;: &quot;gallery-1&quot; }">
+                    <span style="background-image: url('assets/images/pic3.jpg')">
+                    </span>
+                  </figure>              </div>
+                <div class="col aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+                  <figure class="media media-image equal-md-1-2" data-bp="{&quot;imgSrc&quot;: &quot;./assets/images/startup-4.jpg&quot; , &quot;parentGalleryClass&quot;: &quot;gallery-1&quot; }">
+                    <span style="background-image: url('https://cube.webuildthemes.com/assets/images/startup-4.jpg')">
+                    </span>
+                  </figure>              </div>
+              </div>
+            </div>
+            <div class="col-lg-8 col-xl-5">
+              <h2>Get connected to the right lender</h2>
+              <p class="text-secondary">GET THE FINANCING YOUR SMALL (72H) OR
+LARGE (7 DAYS) BUSINESS NEEDS
+FROM $5000 - $5,000,000</p>
+              <div class="row g-0">
+                <div class="col-md-6">
+                  <ul class="list-group list-group-minimal">
+                    <li class="list-group-item d-flex align-items-center mb-1">
+                      <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-2"><i class="bi bi-check2 text-primary"></i>
+                      </div>
+                      High rate of approval
+                    </li>
+                    <li class="list-group-item d-flex align-items-center mb-1">
+                      <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-2"><i class="bi bi-check2 text-primary"></i>
+                      </div>
+                      Get funds fast
+                    </li>
+                    <li class="list-group-item d-flex align-items-center mb-1">
+                      <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-2"><i class="bi bi-check2 text-primary"></i>
+                      </div>
+                      No credit is required
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-md-6">
+                  <ul class="list-group list-group-minimal">
+                    <li class="list-group-item d-flex align-items-center mb-1">
+                      <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-2"><i class="bi bi-check2 text-primary"></i>
+                      </div>
+                      Get the money you need
+                    </li>
+                    <li class="list-group-item d-flex align-items-center mb-1">
+                      <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-2"><i class="bi bi-check2 text-primary"></i>
+                      </div>
+                      Solutions from one lender
+                    </li>
+                    <li class="list-group-item d-flex align-items-center mb-1">
+                      <div class="icon-box icon-box-sm bg-opaque-primary rounded-circle me-2"><i class="bi bi-check2 text-primary"></i>
+                      </div>
+                      Personal assistance
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
